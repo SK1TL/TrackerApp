d405358d44719f9de552e6e81fbb862ea4591040
Artur <sssk1tlll@yandex.ru>
2024-07-21 23:09:53 +0300

    Корректировкаэ

--- TrackerApp/Application/AppDelegate.swift
+++ TrackerApp/Application/AppDelegate.swift
@@ -13,7 +13,7 @@ import YandexMobileMetrica
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     lazy var persistentContainer: NSPersistentContainer = {
-            let container = NSPersistentContainer(name: "Tracker")
+            let container = NSPersistentContainer(name: "TrackerApp")
             container.loadPersistentStores(completionHandler: { (storeDescription, error) in
                 if let error = error as NSError? {
                     fatalError("Unresolved error \(error), \(error.userInfo)")
